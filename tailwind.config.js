572aaa7b619197117d1b75c249ffc42f272fc419
nipeloe022 <nipeloe022@student.wethinkcode.co.za>
2023-09-08 10:59:16 +0200

    added responsiveness for main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,29 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+
       height:{
         '10v': '10vh',
+        '15v': '15vh',
         '20v': '20vh',
         '50v': '50vh',
         '80v': '80vh',
         '90v': '90vh'
       },
-      
+
+      minHeight:{
+        '80v': '80vh',
+        '90v': '90vh',
+      },
+
+      margin:{
+        '40%': '40%'
+      },
+
+      backgroundImage:{
+        'hero': "url('../src/img/back.jpg')",
+        'hero-small': '',
+      }
     
     },
   },
