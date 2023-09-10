eed313a8dc6430eb46e0a69b70ea2023fcc88e9e
nipeloe022 <nipeloe022@student.wethinkcode.co.za>
2023-09-10 10:02:25 +0200

    Fixed the sizes of the mobile options

--- home.js
+++ home.js
@@ -2,7 +2,6 @@ console.log('legend');
 
 // Pen
 const view_pen = document.getElementById('view_pen');
-const displayPen = 'http://127.0.0.1:5500/src/img/manta.jpg';
 
 const pen_1 = document.getElementById('pen_1');
 const pen_2 = document.getElementById('pen_2');
@@ -13,27 +12,20 @@ const showMorePen = document.getElementById('more_pen');
 
 //Pen event handlers
 pen_1.addEventListener('click', () => {
-    if(displayPen === view_pen.src){
-        view_pen.setAttribute('src', pen_1.src);
-        pen_1.setAttribute('src', displayPen);
-    }
-    else{
-        pen_1.setAttribute('src', view_pen.src);
-        view_pen.setAttribute('src', displayPen);
-    }
+    const display = view_pen.src;
+    view_pen.setAttribute('src', pen_1.src);
+    pen_1.setAttribute('src', display);
+
 })
 
 pen_2.addEventListener('click', () => {
-    if(displayPen === view_pen.src){
-        view_pen.setAttribute('src', pen_2.src);
-        pen_2.setAttribute('src', displayPen);
-    }
-    else{
-        pen_2.setAttribute('src', view_pen.src);
-        view_pen.setAttribute('src', displayPen);
-    }
+    const display = view_pen.src;
+    view_pen.setAttribute('src', pen_2.src);
+    pen_2.setAttribute('src', display);
+
 })
 
+//MOBILE OPTIONS
 penBtn.addEventListener('click', () => {
     showMorePen.style.display = 'block';
     showMorePen.style.position = 'absolute';
@@ -42,9 +34,29 @@ penBtn.addEventListener('click', () => {
 })
 
 
+const pen_11 = document.getElementById('pen_11');
+const pen_22 = document.getElementById('pen_22');
+
+pen_11.addEventListener('click', () => {
+    console.log('kota')
+    const display = view_pen.src;
+    view_pen.setAttribute('src', pen_11.src);
+    pen_11.setAttribute('src', display);
+    showMorePen.style.display = 'none';
+})
+
+
+pen_22.addEventListener('click', () => {
+    console.log('kota')
+    const display = view_pen.src;
+    view_pen.setAttribute('src', pen_22.src);
+    pen_22.setAttribute('src', display);
+    showMorePen.style.display = 'none';
+})
+
+
 // Pencil
 const view_pencil = document.getElementById('view_pencil');
-const displayPencil = 'http://127.0.0.1:5500/src/img/lwando.jpg';
 
 const pencil_1 = document.getElementById('pencil_1');
 const pencil_2 = document.getElementById('pencil_2');
@@ -53,40 +65,23 @@ const pencil_3 = document.getElementById('pencil_3');
 const showMorePencil = document.getElementById('more_pencil');
 const pencilBtn = document.getElementById('pencilBtn');
 
+//Pencil Handlers
 pencil_1.addEventListener('click', () => {
-    if(displayPencil === view_pencil.src){
-        view_pencil.setAttribute('src', pencil_1.src);
-        pencil_1.setAttribute('src', displayPencil);
-    }
-    else{
-        pencil_1.setAttribute('src', view_pencil.src);
-        view_pencil.setAttribute('src', displayPencil);
-    }
-
+    const display = view_pencil.src;
+    view_pencil.setAttribute('src', pencil_1.src);
+    pencil_1.setAttribute('src', display);
 })
 
 pencil_2.addEventListener('click', () => {
-    if(displayPencil === view_pencil.src){
-        view_pencil.setAttribute('src', pencil_2.src);
-        pencil_2.setAttribute('src', displayPencil);
-    }
-    else{
-        pencil_2.setAttribute('src', view_pencil.src);
-        view_pencil.setAttribute('src', displayPencil);
-    }
-
+    const display = view_pencil.src;
+    view_pencil.setAttribute('src', pencil_2.src);
+    pencil_2.setAttribute('src', display);
 })
 
 pencil_3.addEventListener('click', () => {
-    if(displayPencil === view_pencil.src){
-        view_pencil.setAttribute('src', pencil_3.src);
-        pencil_3.setAttribute('src', displayPencil);
-    }
-    else{
-        pencil_3.setAttribute('src', view_pencil.src);
-        view_pencil.setAttribute('src', displayPencil);
-    }
-
+    const display = view_pencil.src;
+    view_pencil.setAttribute('src', pencil_3.src);
+    pencil_3.setAttribute('src', display);
 })
 
 pencilBtn.addEventListener('click', () => {
@@ -97,3 +92,23 @@ pencilBtn.addEventListener('click', () => {
 
 })
 
+
+//MOBILE OPTIONS
+const pencil_11 = document.getElementById('pencil_11');
+const pencil_22 = document.getElementById('pencil_22');
+
+pencil_11.addEventListener('click', () => {
+    console.log('kota')
+    const display = view_pencil.src;
+    view_pencil.setAttribute('src', pencil_11.src);
+    pencil_11.setAttribute('src', display);
+    showMorePencil.style.display = 'none';
+})
+
+pencil_22.addEventListener('click', () => {
+    console.log('kota')
+    const display = view_pencil.src;
+    view_pencil.setAttribute('src', pencil_22.src);
+    pencil_22.setAttribute('src', display);
+    showMorePencil.style.display = 'none';
+})
